d1aadd59a76a7091521c9464860408d65318a0bf
Giorgos Kalomalos <Giorgos.Kalomalos@Peoplecert.onmicrosoft.com>
2018-10-28 12:31:38 +0200

    Convert user input (string) to int

--- MyCalculator/Program.cs
+++ MyCalculator/Program.cs
@@ -10,12 +10,54 @@ namespace MyCalculator
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            int num1 = 0; int num2 = 0;
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\n");
+
+            Console.WriteLine("Type a number, and then press Enter");
+            num1 = Convert.ToInt32(Console.ReadLine());   //used Convert.ToInt32 to convert the string the user inputs to int
+
+            Console.WriteLine("Type another number, and then press Enter");
+            num2 = Convert.ToInt32(Console.ReadLine());   //used Convert.ToInt32 to convert the string the user inputs to int
+
+            // Ask the user to choose an option
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your option? ");
+
+            // Use a switch statement to do the math
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.WriteLine("Your result: {num1} + {num2} = " + (num1 + num2)); // Line 32
+                    break;
+                case "s":
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "m":
+                    Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                    break;
+                case "d":
+                    // Ask the user to enter a non-zero divisor until they do so
+                    while (num2 == 0)
+                    {
+                        Console.WriteLine("Enter a non-zero divisor: ");
+                        num2 = Convert.ToInt16(Console.ReadLine());
+                    }
+                    Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                    break;
+                // Return text for an incorrect option entry
+                default:
+                    Console.WriteLine("That is an incorrect option entry, please try again.");
+                    break;
+            }
+            // Wait for the user to respond before closing
+            Console.Write("Press any key to close the Calculator console app...");
+            Console.ReadKey();
         }
     }
 }
